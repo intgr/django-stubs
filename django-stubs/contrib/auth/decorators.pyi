e65fb157d12c70d165703f3af4370a99d4cdfb6b
Marti Raudsepp <marti@juffo.org>
2020-01-23 21:37:08 +0200

    Make decorator functions transparent to Mypy
    By declaring return type as -> Callable[[_FUNC], _FUNC], Mypy can infer
    that the decorated function has also the same arguments and return type
    as the original.
    
    View functions are constrained to return HttpResponseBase (or any
    subclass of it).

--- django-stubs/contrib/auth/decorators.pyi
+++ django-stubs/contrib/auth/decorators.pyi
@@ -3,15 +3,17 @@ from typing import Callable, List, Optional, Set, Union, TypeVar, overload, Prot
 from django.contrib.auth import REDIRECT_FIELD_NAME as REDIRECT_FIELD_NAME  # noqa: F401
 from django.http.response import HttpResponseBase
 
+from django.contrib.auth.models import AbstractUser
+
 _VIEW = TypeVar("_VIEW", bound=Callable[..., HttpResponseBase])
 
 def user_passes_test(
-    test_func: Callable, login_url: Optional[str] = ..., redirect_field_name: str = ...
+    test_func: Callable[[AbstractUser], bool], login_url: Optional[str] = ..., redirect_field_name: str = ...
 ) -> Callable[[_VIEW], _VIEW]: ...
 
-# There are two ways of calling @login_required
+# There are two ways of calling @login_required: @bare and @with(arguments)
 @overload
-def login_required(function: _VIEW = ...) -> _VIEW: ...
+def login_required(function: _VIEW) -> _VIEW: ...
 @overload
 def login_required(redirect_field_name: str = ..., login_url: Optional[str] = ...) -> Callable[[_VIEW], _VIEW]: ...
 def permission_required(
